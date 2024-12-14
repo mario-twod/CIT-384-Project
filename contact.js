4ea97a61d2bb4d8e8932aafb3f7c27df5944c512
mario-twod <mario.aguilar.cpla@gmail.com>
2024-12-13 20:04:43 -0800

    Add files via upload
    12/13/24

---
+++ contact.js
@@ -0,0 +1,7 @@
+/* querySelector for class use "." and for id use "#" */
+
+const SubButton = document.querySelector('#submitbtn');
+
+button.addEventListener('click', function(){
+   console.log('Button Clicked');
+});
