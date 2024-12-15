4ba43eb5064f57008f304fbea12296adad0898d7
mario-twod <mario.aguilar.cpla@gmail.com>
2024-12-14 21:47:03 -0800

    Delete contact.js

--- contact.js
+++
@@ -1,7 +0,0 @@
-/* querySelector for class use "." and for id use "#" */
-
-const SubButton = document.querySelector('#submitbtn');
-
-button.addEventListener('click', function(){
-   console.log('Button Clicked');
-});
